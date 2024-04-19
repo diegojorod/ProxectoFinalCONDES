895647fadb562cc12c25b49595b7e82ab9d15652
Diego <diegojorod@gmail.com>
2024-04-19 14:14:25 +0200

    V2.0 Probado

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Usuario newUsuario = new Usuario("Diego","Jorge Rodríguez",
+                "diegojorod@gmail.com","abc123.",
+                "722201376", "A Valenzá" );
+
+        CuentaBancaria newCuentaBancaria = new CuentaBancaria("466665841", 1400.00);
+
+        newCuentaBancaria.depositar(1000);
+    }
+}
